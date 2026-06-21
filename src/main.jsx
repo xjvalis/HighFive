fa37ff6543d76dfebea97f9102a6f47847a91287
Jan <xjvalis@gmail.com>
2026-06-21 16:52:08 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
-import { StatusBar } from '@capacitor/status-bar'
 
-const initApp = async () => {
-  try {
-    await StatusBar.setOverlaysWebView({ overlay: true })
-  } catch (e) {
-    // Na webu StatusBar není k dispozici, ignorujeme
-  }
-
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-  )
-}
-
-initApp()
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
