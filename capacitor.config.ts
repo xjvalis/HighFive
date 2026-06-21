8bec145eef22279c665abd2387f22e154b002d8a
Jan <292378605+xjvalis@users.noreply.github.com>
2026-06-21 16:03:29 +0200

    mobile app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.highfive.app',
+  appName: 'HighFive',
+  webDir: 'dist'
+};
+
+export default config;
